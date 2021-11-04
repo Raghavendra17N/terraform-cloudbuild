a66ec391f894e52f088efd60db7bdd6a6b2176d0
Raghavendra17N <raghava01995@gamil.com>
2021-11-04 07:51:42 +0530

    cloud-build-gke1

---
+++ environments/dev/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.86.0"
+    }
+  }
+  backend "gcs" {
+    bucket    =   "${project}-tfstate"
+    prefix    =   "env/dev"
+  }
+  
+}
+
+provider "google" {
+  project     = var.project
+  region      = var.region
+  
+}
+
+module "gke" {
+
+  source = "./module"
+  name                  = "gke-pratice-1"
+  location              = "us-central1-a"
+  gke_service_acct      = "83608490393-compute@developer.gserviceaccount.com"
+  #node_location        = ["us-central1-a"]
+  project               = var.project
+  network               = "default"
+  #initial_node_count  = 1
+}
---
+++ environments/dev/variable.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  type    = string
+  default = "nice-proposal-229011"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
